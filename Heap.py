34e75147799d5d831844065b2cec1f3b430444e4
Alexandrion2311 <31349836+Alexandrion2311@users.noreply.github.com>
2017-08-28 16:48:44 +0300

    Update Heap.py

--- Heap.py
+++ Heap.py
@@ -3,19 +3,20 @@ import unittest
 
 class Heap(object):
     def __init__(self, v):
-        self.x = [0]*v
+        self.x = [0] * v
         self.n = 0
 
     def size(self):
         return self.n
 
     def sift_up(self, a):
-        self.x[self.n] = a
-        self.n += 1
-        c = self.n - 1
-        while c != 0 and self.x[c] > self.x[(c-1) // 2]:
-            self.x[c], self.x[(c-1) // 2] = self.x[(c-1) // 2], self.x[c]
-            c = (c-1) // 2
+        if (self.n) != len(self.x):
+            self.x[self.n] = a
+            self.n += 1
+            c = self.n - 1
+            while c != 0 and self.x[c] > self.x[(c - 1) // 2]:
+                self.x[c], self.x[(c - 1) // 2] = self.x[(c - 1) // 2], self.x[c]
+                c = (c - 1) // 2
 
     def top(self):
         return self.x[0]
@@ -30,12 +31,12 @@ class Heap(object):
             p = 0
             c1 = 1
             c2 = 2
-            while self.x[p] < self.x[c1] or self.x[p] < self.x[c2]:
+            while (c1 + 1) < len(self.x) and (self.x[p] < self.x[c1] or self.x[p] < self.x[c2]):
                 if self.x[c1] >= self.x[c2]:
                     self.x[p], self.x[c1] = self.x[c1], self.x[p]
                     p = c1
-                    c1 = p*2 + 1
-                    c2 = p*2 + 2
+                    c1 = p * 2 + 1
+                    c2 = p * 2 + 2
                 else:
                     self.x[p], self.x[c2] = self.x[c2], self.x[p]
                     p = c2
@@ -44,7 +45,6 @@ class Heap(object):
 
 
 class TestHEAP(unittest.TestCase):
-
     def test_HEAP00(self):
         k = Heap(10)
         self.assertEqual(k.size(), 0)
@@ -62,7 +62,7 @@ class TestHEAP(unittest.TestCase):
         k.extract()
         k.extract()
         self.assertEqual(k.top(), 0)
-        
+
     def test_heap_01(self):
         heap = Heap(2)
         heap.sift_up(1)
@@ -70,6 +70,16 @@ class TestHEAP(unittest.TestCase):
         self.assertEqual(heap.top(), 2)
         heap.extract()
         self.assertEqual(heap.top(), 1)
+
+    def test_heap_02(self):
+        heap = Heap(2)
+        heap.sift_up(1)
+        heap.sift_up(2)
+        heap.sift_up(3)
+        self.assertEqual(heap.top(), 2)
+        heap.extract()
+        self.assertEqual(heap.top(), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
